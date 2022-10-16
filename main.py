c8fa2b2053cde0a056d7ef261933df0d22649251
Julio Olmedo <turlan125@gmail.com>
2022-10-16 17:49:53 -0300

    add create tables if not exists

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 from app.v1.router.user_router import router as user_router
 from app.v1.router.todo_router import router as todo_router
 
+from app.v1.scripts.create_tables import create_tables
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(todo_router)
+create_tables()
 
 
 @app.get('/')
